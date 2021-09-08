e944f8e55d843424a6bb8d1f486cc823dfe8a93c
Holstein30 <holstein30@live.com>
2021-09-08 09:31:03 -0400

    SearchBar component initial setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -24,6 +25,7 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
+      <SearchBar />
       <Grid header="Popular Movies">
         {state.results.map((movie) => (
           <Thumb
