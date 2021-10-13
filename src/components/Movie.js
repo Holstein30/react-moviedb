0be218aaf1717b900a179d16e5f705a09fb51c69
Holstein30 <holstein30@live.com>
2021-10-13 14:45:11 -0400

    Movie component initial setup

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
 import React from "react";
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
-const Movie = () => <div>Movie</div>;
+// Components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+
+// Hook
+
+// Image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
